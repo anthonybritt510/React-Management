622ebb616dacb7c0d72a36757dda28810a7e9e92
anthonybritt510 <54913046+anthonybritt510@users.noreply.github.com>
2020-03-15 18:28:34 +0100

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} alt="Logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': 'Jovan Markovic',
+  'birthday': '1988-08-09',
+  'gender': 'Male',
+  'job': 'Senior Web Developer'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': 'Priyanka Hinal',
+  'birthday': '1988-08-09',
+  'gender': 'Female',
+  'job': 'Team Manager'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': 'Rakesh Maxim',
+  'birthday': '1988-08-09',
+  'gender': 'Male',
+  'job': 'Senior Web Developer'
+},
+{
+  'id': 4,
+  'image': 'https://placeimg.com/64/64/4',
+  'name': 'Jacek Betal',
+  'birthday': '1988-08-09',
+  'gender': 'Male',
+  'job': 'Designer'
+}]
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
